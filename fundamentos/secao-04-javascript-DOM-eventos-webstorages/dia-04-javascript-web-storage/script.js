7f416bdf4e9a6cdab47f6f2db98afce626ab69a3
Frederico Brion <fredericoasbrion@gmail.com>
2023-04-13 15:51:28 -0300

    colocando o window.onload

--- fundamentos/secao-04-javascript-DOM-eventos-webstorages/dia-04-javascript-web-storage/script.js
+++ fundamentos/secao-04-javascript-DOM-eventos-webstorages/dia-04-javascript-web-storage/script.js
@@ -1,67 +1,71 @@
-const main = document.querySelector('main');
+window.onload = () => {
+  const main = document.querySelector("main");
 
-const changeBackground = () => {
-    const backgroundButton = document.querySelectorAll('#background-color button');
+  const changeBackground = () => {
+    const backgroundButton = document.querySelectorAll(
+      "#background-color button"
+    );
     for (let index of backgroundButton) {
-        index.addEventListener('click', () => {
-            localStorage.setItem('background', index.innerText);
-            main.style.backgroundColor = index.innerText;
-        })
+      index.addEventListener("click", () => {
+        localStorage.setItem("background", index.innerText);
+        main.style.backgroundColor = index.innerText;
+      });
     }
-};
+  };
 
-const changeFontColor = () => {
-    const fontColorButton = document.querySelectorAll('#font-color button');
+  const changeFontColor = () => {
+    const fontColorButton = document.querySelectorAll("#font-color button");
     for (let index of fontColorButton) {
-        index.addEventListener('click', () => {
-            localStorage.setItem('fontColor', index.innerText);
-            main.style.color = index.innerText;
-        })
+      index.addEventListener("click", () => {
+        localStorage.setItem("fontColor", index.innerText);
+        main.style.color = index.innerText;
+      });
     }
-};
+  };
 
-const changeFontSize = () => {
-    const fontSizeButton = document.querySelectorAll('#font-size button');
+  const changeFontSize = () => {
+    const fontSizeButton = document.querySelectorAll("#font-size button");
     for (let index of fontSizeButton) {
-        index.addEventListener('click', () => {
-            localStorage.setItem('fontSize', index.innerText);
-            main.style.fontSize = index.innerText;
-        })
+      index.addEventListener("click", () => {
+        localStorage.setItem("fontSize", index.innerText);
+        main.style.fontSize = index.innerText;
+      });
     }
-};
+  };
 
-const changeLineHeight = () => {
-    const lineHeightButton = document.querySelectorAll('#line-height button');
+  const changeLineHeight = () => {
+    const lineHeightButton = document.querySelectorAll("#line-height button");
     for (let index of lineHeightButton) {
-        index.addEventListener('click', () => {
-            localStorage.setItem('lineHeight', index.innerText);
-            main.style.lineHeight = index.innerText;
-        })
+      index.addEventListener("click", () => {
+        localStorage.setItem("lineHeight", index.innerText);
+        main.style.lineHeight = index.innerText;
+      });
     }
-};
+  };
 
-const changeFontStyle = () => {
-    const fontStyleButton = document.querySelectorAll('#font-family button');
+  const changeFontStyle = () => {
+    const fontStyleButton = document.querySelectorAll("#font-family button");
     for (let index of fontStyleButton) {
-        index.addEventListener('click', () => {
-            localStorage.setItem('fontStyle', index.innerText);
-            main.style.fontFamily = index.innerText;
-        })
+      index.addEventListener("click", () => {
+        localStorage.setItem("fontStyle", index.innerText);
+        main.style.fontFamily = index.innerText;
+      });
     }
+  };
+
+  changeBackground();
+  changeFontColor();
+  changeFontSize();
+  changeLineHeight();
+  changeFontStyle();
+
+  const initialize = () => {
+    main.style.backgroundColor = localStorage.getItem("background");
+    main.style.color = localStorage.getItem("fontColor");
+    main.style.fontSize = localStorage.getItem("fontSize");
+    main.style.lineHeight = localStorage.getItem("lineHeight");
+    main.style.fontFamily = localStorage.getItem("fontStyle");
+  };
+
+  initialize();
 };
-
-changeBackground();
-changeFontColor();
-changeFontSize();
-changeLineHeight();
-changeFontStyle();
-
-const initialize = () => {
-    main.style.backgroundColor = localStorage.getItem('background');
-    main.style.color = localStorage.getItem('fontColor');
-    main.style.fontSize = localStorage.getItem('fontSize');
-    main.style.lineHeight = localStorage.getItem('lineHeight');
-    main.style.fontFamily = localStorage.getItem('fontStyle');
-};
-
-initialize();
