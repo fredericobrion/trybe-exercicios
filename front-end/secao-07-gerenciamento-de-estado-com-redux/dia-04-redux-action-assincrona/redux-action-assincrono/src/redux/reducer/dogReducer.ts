64fbf69c4a31a2dbb56207d53679baa4ca74eb97
Frederico Brion <fredericoasbrion@gmail.com>
2023-07-17 11:02:16 -0300

    finalizando dia 7.04

---
+++ front-end/secao-07-gerenciamento-de-estado-com-redux/dia-04-redux-action-assincrona/redux-action-assincrono/src/redux/reducer/dogReducer.ts
@@ -0,0 +1,49 @@
+import {
+  REQUEST_FAILED,
+  REQUEST_STARTED,
+  REQUEST_SUCCESSFUL,
+} from '../actions';
+
+const initialState = {
+  isFetching: false,
+  imageURL: '',
+  errorMessage: '',
+};
+
+type ActionType = {
+  payload: string;
+  type: string;
+};
+
+function dogReducer(state = initialState, action: ActionType) {
+  switch (action.type) {
+    case REQUEST_STARTED:
+      return {
+        ...state,
+        isFetching: true,
+        errorMessage: '',
+        imageURL: '',
+      };
+
+    case REQUEST_SUCCESSFUL:
+      return {
+        ...state,
+        isFetching: false,
+        imageURL: action.payload,
+        errorMessage: '',
+      };
+
+    case REQUEST_FAILED:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload,
+        imageURL: '',
+      };
+
+    default:
+      return state;
+  }
+}
+
+export default dogReducer;
